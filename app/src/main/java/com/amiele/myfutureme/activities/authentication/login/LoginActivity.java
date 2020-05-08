18b1a43e7c925ed63f3ce06fb6ca7d2886928dc5
Amie <leduo@deakin.edu.au>
2020-05-08 20:48:19 +1000

    Initial commit

---
+++ app/src/main/java/com/amiele/myfutureme/activities/authentication/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.amiele.myfutureme.activities.authentication.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amiele.myfutureme.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/amiele/myfutureme/activities/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.amiele.myfutureme.activities.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amiele.myfutureme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
