2916e215b36b8f3089ce53c6a6034cae84c0fa52
Amie <leduo@deakin.edu.au>
2020-05-08 23:17:38 +1000

    Add Login page

--- app/src/main/java/com/amiele/myfutureme/activities/authentication/login/LoginActivity.java
+++ app/src/main/java/com/amiele/myfutureme/activities/authentication/login/LoginActivity.java
@@ -2,9 +2,12 @@ package com.amiele.myfutureme.activities.authentication.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.amiele.myfutureme.R;
+import com.amiele.myfutureme.activities.main.MainActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginBtnClicked(View view)
+    {
+        Intent mainActivity = new Intent(this, MainActivity.class);
+        startActivity(mainActivity);
     }
 }
