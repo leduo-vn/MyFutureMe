bb1c2e2402272d125ad6a2d4964c4e997e4231c9
Amie <leduo@deakin.edu.au>
2020-05-27 02:21:08 +1000

    add Filter for tag

--- app/src/main/java/com/amiele/myfutureme/activities/work/AddTagActivity.java
+++ app/src/main/java/com/amiele/myfutureme/activities/work/AddTagActivity.java
@@ -1,13 +1,20 @@
 package com.amiele.myfutureme.activities.work;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -23,12 +30,15 @@ public class AddTagActivity extends AppCompatActivity {
     ArrayList<Tag> tagList = new ArrayList<>();
     TagAdapter adapter;
     TextView tvTagName;
+    LinearLayout llAddTag;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tag);
+
+        llAddTag = findViewById(R.id.add_tag_layout);
 
         tagList.add(new Tag("Friend",Color.parseColor("#EEDBAA")));
         tagList.add(new Tag("LifeStyle",Color.parseColor("#BDEEAA")));
@@ -59,6 +69,8 @@ public class AddTagActivity extends AppCompatActivity {
                 AddTag();
             }
         });
+
+
     }
 
     private void OpenColorPicker()
@@ -100,8 +112,60 @@ public class AddTagActivity extends AppCompatActivity {
         String name = tvTagName.getText().toString();
         ColorDrawable color = (ColorDrawable) tvTagName.getBackground();
         tagList.add(new Tag(name,color.getColor()));
-        adapter.notifyItemInserted(tagList.size()-1);
-//        adapter.notifyDataSetChanged();
+//        adapter.notifyItemInserted(tagList.size()-1);
+        adapter.setTagList(tagList);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_tag_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        MenuItem doneItem = menu.findItem(R.id.action_done);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String filteredText) {
+                adapter.getFilter().filter(filteredText);
+                return false;
+            }
+        });
+
+        searchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                llAddTag.setVisibility(View.GONE);
+                doneItem.setVisible(false);
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                llAddTag.setVisibility(View.VISIBLE);
+                doneItem.setVisible(true);
+                invalidateOptionsMenu();
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_done:
+                Toast.makeText(this, "Done selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     private  void DisplayToast(String text)
     {
--- app/src/main/java/com/amiele/myfutureme/activities/work/TagAdapter.java
+++ app/src/main/java/com/amiele/myfutureme/activities/work/TagAdapter.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,10 +24,51 @@ import com.amiele.myfutureme.R;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.List;
 
-public class TagAdapter  extends RecyclerView.Adapter<TagAdapter.TagViewHolder> {
+public class TagAdapter  extends RecyclerView.Adapter<TagAdapter.TagViewHolder> implements Filterable {
+    private ArrayList<Tag> tagListOriginal;
     private ArrayList<Tag> tagList;
     private static TagAdapter.OnItemClickListener listener= null;
+
+    @Override
+    public Filter getFilter() {
+        return tagListFilter;
+    }
+
+    private Filter tagListFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Tag> filteredTagList = new ArrayList<>();
+            FilterResults filterResults = new FilterResults();
+
+            if (constraint==null|| constraint.length()==0)
+            {
+                filteredTagList.addAll(tagListOriginal);
+            }
+            else
+            {
+                String pattern = constraint.toString();
+                pattern.toLowerCase().trim();
+
+                for (Tag tag:tagListOriginal)
+                {
+                    if (tag.getName().toLowerCase().trim().contains(pattern)) filteredTagList.add(tag);
+                }
+            }
+
+            filterResults.values = filteredTagList;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            tagList.clear();
+            tagList.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public interface OnItemClickListener {
         void onItemClick(Task task);
@@ -45,13 +88,20 @@ public class TagAdapter  extends RecyclerView.Adapter<TagAdapter.TagViewHolder> 
             super(view);
 
             tvName = itemView.findViewById(R.id.txt_tag_name);
-            btnDeleteTag = itemView.findViewById(R.id.action_delete_tag);
+          //  btnDeleteTag = itemView.findViewById(R.id.action_delete_tag);
            // tvDescription = itemView.findViewById(R.id.txt_sub_task_description);
         }
     }
 
     public TagAdapter(ArrayList<Tag> tagList) {
-        this.tagList = tagList;
+        this.tagListOriginal = tagList;
+        this.tagList = new ArrayList<>(tagList);
+    }
+
+    public void setTagList(ArrayList<Tag> tagList)
+    {
+        this.tagListOriginal = tagList;
+        this.tagList = new ArrayList<>(tagList);
     }
 
     @NonNull
